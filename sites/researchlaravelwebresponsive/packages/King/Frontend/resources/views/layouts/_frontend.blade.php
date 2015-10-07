b3c34df4482323bf7fe2a19bd17759c190e1aa2f
vuong bui <vuong.bui@sutrixmedia.com>
2015-10-07 14:42:10 +0700

    Update css and some meta

--- sites/researchlaravelwebresponsive/packages/King/Frontend/resources/views/layouts/_frontend.blade.php
+++ sites/researchlaravelwebresponsive/packages/King/Frontend/resources/views/layouts/_frontend.blade.php
@@ -3,7 +3,10 @@
     <head>
         <title> @yield('title') - {{ config('front.site_name') }} - {{ config('front.site_desc') }}</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <meta name="msapplication-tap-highlight" content="no"/>
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
         <link rel="stylesheet" href="{{ asset('packages/king/frontend/css/bootstrap.css') }}">
         <link rel="stylesheet" href="{{ asset('packages/king/frontend/css/font-awesome.css') }}">
         @yield('head_css')
