91f41c8697448de2e2390f428dfc0ad44be36f32
vuong bui <vuong.bui@sutrixmedia.com>
2015-10-08 14:24:37 +0700

    Add share product modal

---
+++ sites/researchlaravelwebresponsive/packages/King/Frontend/resources/views/inc/product-share-modal.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade share-product-modal -sp-modal" tabindex="-1" role="dialog" aria-labelledby="shareProductLabel">
+    <div class="modal-dialog">
+        <div class="_fwfl _bgw _r0 modal-content">
+            <div class="_fwfl modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title _tg5 _fs17 " id="shareProductLabel">Share product</h4>
+            </div>
+            <div class="_fwfl modal-body">
+                <form class="_fwfl" action="#" method="GET">
+                    <input type="text" class="_fwfl _r2 share-text-input" placeholder="Inter a name" />
+                </form>
+            </div>
+            <div class="_fwfl modal-footer">
+                <button type="button" class="btn _btn _btn-blue _btn-sm disabled">Tell them</button>
+            </div>
+        </div>
+    </div>
+</div>
